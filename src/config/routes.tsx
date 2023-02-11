cbdc987429a1cd3a3dc526805b71ca94889943bc
TacitusPie <kakmsaikou@gmail.com>
2023-02-11 22:07:30 +0800

    解决 edge 浏览器的一些 BUG

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,18 +1,16 @@
-import {Foo} from '../views/Foo';
-import {Bar} from '../views/Bar';
 import {RouteRecordRaw} from 'vue-router';
-import {First} from '../components/welcome/first';
-import {Second} from '../components/welcome/second';
-import {Third} from '../components/welcome/third';
-import {Fourth} from '../components/welcome/fourth';
+import {First} from '../components/welcome/First';
+import {Second} from '../components/welcome/Second';
+import {Third} from '../components/welcome/Third';
+import {Fourth} from '../components/welcome/Fourth';
 import {Welcome} from '../views/Welcome';
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Foo},
-  { path: '/about', component: Bar},
+  { path: '/', redirect: './welcome'},
   { path: '/welcome',
     component: Welcome,
     children: [
+      {path:'', redirect: '/welcome/1'},
       {path: '1', component: First},
       {path: '2', component: Second},
       {path: '3', component: Third},
