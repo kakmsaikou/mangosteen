446b0ce512de88d0fe310c55de987d80ff96c8f8
TacitusPie <kakmsaikou@gmail.com>
2023-02-12 20:45:59 +0800

    四个欢迎页面

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,6 +1,6 @@
 import {defineComponent} from 'vue';
 import s from './First.module.scss'
-import piggy from '../../assets/icons/piggy.svg'
+import icon from '../../assets/icons/piggy.svg'
 import {RouterLink} from 'vue-router';
 
 export const First = defineComponent({
@@ -8,12 +8,12 @@ export const First = defineComponent({
     return ()=>(
       <div class={s.wrapper}>
         <div class={s.card}>
-          <img src={piggy} alt=""/>
+          <img src={icon} alt=""/>
           <h2>会挣钱<br/>还要会省钱</h2>
         </div>
         <div class={s.actions}>
           <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
-          <RouterLink to='welcome/2'>下一页</RouterLink>
+          <RouterLink to='/welcome/2'>下一页</RouterLink>
           <RouterLink to='/start'>跳过</RouterLink>
         </div>
       </div>
