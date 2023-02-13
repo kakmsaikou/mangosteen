11c5445e724656bcad702d16615a4e51409a486d
TacitusPie <kakmsaikou@gmail.com>
2023-02-13 22:34:14 +0800

    添加 useSwipe.tsx

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,9 +1,15 @@
-import {defineComponent, VNode, Transition} from 'vue';
+import {defineComponent, VNode, Transition, ref, watchEffect} from 'vue';
 import {RouteLocationNormalizedLoaded, RouterView} from 'vue-router';
 import s from './Welcome.module.scss';
+import {useSwipe} from '../hooks/useSwipe';
 
 export const Welcome = defineComponent({
   setup: () => {
+    const main = ref<HTMLElement | null>(null)
+    const {direction} = useSwipe(main)
+    watchEffect(()=>{
+      console.log(direction.value)
+    })
     return () =>
       <div class={s.wrapper}>
         <header>
@@ -12,7 +18,7 @@ export const Welcome = defineComponent({
           </svg>
           <h1>山竹记账</h1>
         </header>
-        <main class={s.main}>
+        <main class={s.main} ref={main}>
           <RouterView name="main">
             {({Component: Content, route: R}: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
               <Transition
