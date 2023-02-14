713d22cb73af5209b12036e99885bc316b2a5678
TacitusPie <kakmsaikou@gmail.com>
2023-02-14 13:05:30 +0800

    四张欢迎卡片的切换

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,9 +1,33 @@
-import {defineComponent, VNode, Transition} from 'vue';
-import {RouteLocationNormalizedLoaded, RouterView} from 'vue-router';
+import {defineComponent, VNode, Transition, ref, watchEffect} from 'vue';
+import {RouteLocationNormalizedLoaded, RouterView, useRoute, useRouter} from 'vue-router';
 import s from './Welcome.module.scss';
+import {useSwipe} from '../hooks/useSwipe';
+import {throttle} from '../shared/throttle';
 
 export const Welcome = defineComponent({
   setup: () => {
+    const main = ref<HTMLElement>();
+    const {direction, swiping} = useSwipe(main, {
+      beforeStart: (e) => e.preventDefault()
+    });
+    const route = useRoute();
+    const router = useRouter();
+    const push = throttle(()=>{
+      if (route.name === 'Welcome1') {
+        router.push('/welcome/2');
+      }else if (route.name === 'Welcome2') {
+        router.push('/welcome/3');
+      }else if(route.name === 'Welcome3'){
+        router.push('/welcome/4')
+      }else if(route.name === 'Welcome4'){
+        router.push('/start')
+      }
+    }, 500)
+    watchEffect(() => {
+      if (swiping.value && direction.value === 'left') {
+        push()
+      }
+    });
     return () =>
       <div class={s.wrapper}>
         <header>
@@ -12,7 +36,7 @@ export const Welcome = defineComponent({
           </svg>
           <h1>山竹记账</h1>
         </header>
-        <main class={s.main}>
+        <main class={s.main} ref={main}>
           <RouterView name="main">
             {({Component: Content, route: R}: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
               <Transition
