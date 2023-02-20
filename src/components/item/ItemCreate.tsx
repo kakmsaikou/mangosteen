902a47eee07901c5324f9e46b71c125f87f8921c
TacitusPie <kakmsaikou@gmail.com>
2023-02-20 14:51:16 +0800

    完成 tab 切换

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,16 +1,26 @@
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 import s from './ItemCreate.module.scss'
 import {Icon} from '../../shared/Icon';
 import {MainLayout} from '../../layouts/MainLayout';
+import {Tabs, Tab} from '../../shared/Tabs';
 
 export const ItemCreate = defineComponent({
   setup: ()=>{
+    const refSelectedType = ref('支出')
     return ()=>(
       <MainLayout>{{
           title: ()=> '记一笔',
           icon: ()=> <Icon name='left' class={s.navIcon} />,
           default: ()=><>
-            main
+            <Tabs selected={refSelectedType.value}
+                  onUpdateSelected={(type)=>{refSelectedType.value = type}}>
+              <Tab name='支出'>
+                icon 列表
+              </Tab>
+              <Tab name='收入'>
+                icon 列表2
+              </Tab>
+            </Tabs>
           </>
         }}</MainLayout>
     )
