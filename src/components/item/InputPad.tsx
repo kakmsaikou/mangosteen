0066b9bcdb4899b0a2596d16061a2d224e1185ab
TacitusPie <kakmsaikou@gmail.com>
2023-02-21 12:27:03 +0800

    引入vant实现日期选择

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,42 +1,52 @@
-import {defineComponent} from 'vue';
-import s from './InputPad.module.scss';
+import {defineComponent, ref} from 'vue';
 import {Icon} from '../../shared/Icon';
+import {time} from '../../shared/time';
+import s from './InputPad.module.scss';
+import {DatetimePicker, Popup} from 'vant';
 
 export const InputPad = defineComponent({
   setup: () => {
-    return () => {
-      const buttons = [
-        {text: '1', onClick: () => {}},
-        {text: '2', onClick: () => {}},
-        {text: '3', onClick: () => {}},
-        {text: '清空', onClick: () => {}},
-        {text: '4', onClick: () => {}},
-        {text: '5', onClick: () => {}},
-        {text: '6', onClick: () => {}},
-        {text: '+', onClick: () => {}},
-        {text: '7', onClick: () => {}},
-        {text: '8', onClick: () => {}},
-        {text: '9', onClick: () => {}},
-        {text: '-', onClick: () => {}},
-        {text: '.', onClick: () => {}},
-        {text: '0', onClick: () => {}},
-        {text: '删', onClick: () => {}},
-        {text: '提交', onClick: () => {}},
-      ];
-      return (<>
-        <div class={s.dateAmountContainer}>
+    const now = new Date();
+    const refDate = ref<Date>(now);
+    const buttons = [
+      {text: '1', onClick: () => {}},
+      {text: '2', onClick: () => {}},
+      {text: '3', onClick: () => {}},
+      {text: '清空', onClick: () => {}},
+      {text: '4', onClick: () => {}},
+      {text: '5', onClick: () => {}},
+      {text: '6', onClick: () => {}},
+      {text: '+', onClick: () => {}},
+      {text: '7', onClick: () => {}},
+      {text: '8', onClick: () => {}},
+      {text: '9', onClick: () => {}},
+      {text: '-', onClick: () => {}},
+      {text: '.', onClick: () => {}},
+      {text: '0', onClick: () => {}},
+      {text: '删', onClick: () => {}},
+      {text: '提交', onClick: () => {}},
+    ];
+    const refShowPop = ref(false);
+    return () => <>
+      <div class={s.dateAmountContainer}>
           <span class={s.date}>
             <Icon name="date" class={s.icon}/>
-            <span>2022-01-01</span>
+          <span>
+            <span onClick={() => refShowPop.value = true}>{time(refDate.value).format()}</span>
+            <Popup position="bottom" v-model:show={refShowPop.value}>
+              <DatetimePicker v-model={refDate.value} type="date" title="选择年月日"
+                              onConfirm={() => refShowPop.value = false}
+                              onCancel={() => refShowPop.value = false}/>
+            </Popup>
           </span>
-          <span class={s.amount}>1999.2</span>
-        </div>
-        <div class={s.buttons}>
-          {buttons.map(button => (
-            <button onClick={button.onClick}>{button.text}</button>)
-          )}
-        </div>
-      </>);
-    };
+          </span>
+        <span class={s.amount}>1999.2</span>
+      </div>
+      <div class={s.buttons}>
+        {buttons.map(button => (
+          <button onClick={button.onClick}>{button.text}</button>)
+        )}
+      </div>
+    </>;
   }
 });
