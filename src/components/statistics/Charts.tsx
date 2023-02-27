7220b0de624290fd99a3d10460cb5cba88728f53
TacitusPie <kakmsaikou@gmail.com>
2023-02-27 13:12:50 +0800

    complete Chart css

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { computed, defineComponent, onMounted, PropType, reactive, ref } from 'vue';
 import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
 import * as echarts from 'echarts';
@@ -18,6 +18,18 @@ export const Charts = defineComponent({
     const category = ref('expenses');
     const refDiv = ref<HTMLDivElement>();
     const refDiv2 = ref<HTMLDivElement>();
+    const data3 = reactive([
+      { tag: { id: 1, name: '房租', sign: 'x' }, amount: 3000 },
+      { tag: { id: 1, name: '房租', sign: 'x' }, amount: 3000 },
+      { tag: { id: 1, name: '房租', sign: 'x' }, amount: 3000 },
+    ]);
+    const betterData3 = computed(() => {
+      const total = data3.reduce((sum, item) => sum + item.amount, 0);
+      return data3.map(item => ({
+        ...item,
+        percent: Math.round((item.amount / total) * 100) + '%',
+      }));
+    });
     onMounted(() => {
       if (refDiv.value === undefined) return;
       const myChart = echarts.init(refDiv.value);
@@ -67,17 +79,17 @@ export const Charts = defineComponent({
               { value: 735, name: 'Direct' },
               { value: 580, name: 'Email' },
               { value: 484, name: 'Union Ads' },
-              { value: 300, name: 'Video Ads' }
+              { value: 300, name: 'Video Ads' },
             ],
             emphasis: {
               itemStyle: {
                 shadowBlur: 10,
                 shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
-            }
-          }
-        ]
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+              },
+            },
+          },
+        ],
       });
     });
     return () => (
@@ -93,6 +105,24 @@ export const Charts = defineComponent({
         />
         <div class={s.demo} ref={refDiv}></div>
         <div class={s.demo2} ref={refDiv2}></div>
+        <div class={s.demo3}>
+          {betterData3.value.map(({ tag, amount, percent }) => {
+            return (
+              <div class={s.topItem}>
+                <div class={s.sign}>{tag.sign}</div>
+                <div class={s.bar_wrapper}>
+                  <div class={s.bar_text}>
+                    <span>{tag.name} - {percent}</span>
+                    <span>￥{amount}</span>
+                  </div>
+                  <div class={s.bar}>
+                    <div class={s.bar_inner}></div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   },
