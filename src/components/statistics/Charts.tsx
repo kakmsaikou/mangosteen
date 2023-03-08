03ccde1428eef36f390260e4c4efacdf9e75fad2
TacitusPie <kakmsaikou@gmail.com>
2023-03-08 09:36:45 +0800

    敬请期待添加返回按钮

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -45,7 +45,8 @@ export const Charts = defineComponent({
           happen_before: props.endDate,
           kind: kind.value,
           group_by: 'happen_at',
-        }, {
+        },
+        {
           _mock: 'itemSummary',
           _autoLoading: true,
         }
@@ -68,7 +69,8 @@ export const Charts = defineComponent({
           .getTimestamp();
         const item = data1.value[0];
         const amount =
-          item && new Date(item.happen_at).getTime() === time
+          item &&
+          new Date(item.happen_at + 'T00:00:00.000+0800').getTime() === time
             ? data1.value.shift()!.amount
             : 0;
         return [new Date(time).toISOString(), amount];
